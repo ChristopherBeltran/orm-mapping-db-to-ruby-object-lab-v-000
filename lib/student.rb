7b180a3af1539e2ab89b022fc705d10221a3f914
Christopher Beltran <christod.beltran@gmail.com>
2018-10-20 23:51:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,8 +75,8 @@ class Student
     LIMIT 1
     SQL
     DB[:conn].execute(sql).map do |row|
-    self.new_from_db(row)
-    binding.pry
+    first = self.new_from_db(row)
+    return first
   end 
 end
 
